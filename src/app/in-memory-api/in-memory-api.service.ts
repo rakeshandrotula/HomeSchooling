354dff31a044f4adc6289ed22485b4f23f80cbcd
NaveenB <naveen.bandhu@gmail.com>
2020-04-13 10:56:11 +0200

    commit test 2

--- src/app/in-memory-api/in-memory-api.service.ts
+++ src/app/in-memory-api/in-memory-api.service.ts
@@ -41,7 +41,7 @@ export class InMemoryApiService implements InMemoryDbService {
         name: 'German',
         exercises: exercises
       }, {
-        name: 'Dutch - Test',
+        name: 'Dutch - Test 2',
         exercises: exercises
       }];
 
